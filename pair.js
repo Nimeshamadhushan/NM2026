b6530174a338e678e9704307a0b8c8636aaf3e59
Nimeshamadhushan <89959527+Nimeshamadhushan@users.noreply.github.com>
2025-12-29 21:51:48 +0530

    Create pair.js

---
+++ pair.js
@@ -0,0 +1,20 @@
+const { default: makeWASocket, useMultiFileAuthState, delay } = require("@whiskeysockets/baileys");
+const pino = require('pino');
+
+async function getPairCode() {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info');
+    const conn = makeWASocket({
+        auth: state,
+        logger: pino({ level: 'silent' }),
+        browser: ["Ubuntu", "Chrome", "20.0.04"]
+    });
+
+    if (!conn.authState.creds.registered) {
+        await delay(5000);
+        const code = await conn.requestPairingCode("94784776100");
+        console.log("----------------------------");
+        console.log("YOUR CODE IS: " + code);
+        console.log("----------------------------");
+    }
+}
+getPairCode();

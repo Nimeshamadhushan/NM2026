c31c8c94c27325b1050b75fad1401a0474ecc966
Nimeshamadhushan <89959527+Nimeshamadhushan@users.noreply.github.com>
2025-12-29 22:38:52 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'NIMA-V5',
+  author: 'NIMESHA',
+  botName: "NIMA-V5-OFFICIAL",
+  botOwner: 'NIMESHA', // ඔබේ නම
+  ownerNumber: '94726800969', // ඔබේ අංකය
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "2025 නවතම සිංහල WhatsApp රොබෝ.",
+  version: "V5.0.1",
+};
+
+module.exports = settings;
